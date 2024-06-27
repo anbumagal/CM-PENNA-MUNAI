be90d60bb1293c055704f7fef55af82e0d4adaef
anbumagal <148482857+anbumagal@users.noreply.github.com>
2024-06-27 10:56:12 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,163 @@
+import json
+import re
+import random_responses
+from flask import Flask,render_template,request,redirect,url_for
+import sqlite3
+
+app = Flask(__name__)
+chats=[]
+
+# Load JSON data
+def load_json(file):
+    with open(file) as bot_responses:
+        print(f"Loaded '{file}' successfully!")
+        return json.load(bot_responses)
+
+
+# Store JSON data
+response_data = load_json("bot.json")
+
+@app.route("/start",methods=['POST','GET'])
+def start():
+	inp = [str(x) for x in request.form.values()]
+	print(inp[0])
+	result=get_response(inp[0])
+##	#return render_template('chat_bot.html',result=inp[0])
+##	results = model.predict([bag_of_words(inp[0],words)])[0] 
+##	print(results)
+##	results_index = np.argmax(results)
+##	tag = labels[results_index]
+##	print(tag)  
+##
+##	if results[results_index] < 0.8 or len(inp[0])<2:
+##		email="durairaj.renuka@gmail.com"
+##		Msg="Chat bot not able to answer for this question"+str(inp[0])
+##		ms.process(email,Msg)
+##		result ="Sorry, I didn't get you. Please try again. Kindly make live chat to 9500487306"
+##
+##				
+##	else:
+##		for tg in data['intents']:
+##			if tg['tag'] == tag:
+##				responses = tg['responses']
+##
+##		result=""+random.choice(responses)
+##		es= ["Sad to see you go :(", "Talk to you later", "Goodbye!", "See you later","bye-bye"]
+##		if result in es:
+##			email="sai2572000@gmail.com"
+##			Msg="Chat bot not able to answer for this question"+str(inp[0])
+##			ms.process(email,Msg)
+##			result+="Kindly make live chat to 6382621629"
+##			#result ="Sorry, I didn't get you. Please try again."
+
+	chats.append("You: " + inp[0])
+	chats.append(result)
+	return render_template('form.html',chats=chats[::-1],type="")
+
+
+
+
+def get_response(input_string):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', input_string.lower())
+    score_list = []
+
+    # Check all the responses
+    for response in response_data:
+        response_score = 0
+        required_score = 0
+        required_words = response["required_words"]
+
+        # Check if there are any required words
+        if required_words:
+            for word in split_message:
+                if word in required_words:
+                    required_score += 1
+
+        # Amount of required words should match the required score
+        if required_score == len(required_words):
+            # print(required_score == len(required_words))
+            # Check each word the user has typed
+            for word in split_message:
+                # If the word is in the response, add to the score
+                if word in response["user_input"]:
+                    response_score += 1
+
+        # Add score to list
+        score_list.append(response_score)
+        # Debugging: Find the best phrase
+        # print(response_score, response["user_input"])
+
+    # Find the best response and return it if they're not all 0
+    best_response = max(score_list)
+    response_index = score_list.index(best_response)
+
+    # Check if input is empty
+    if input_string == "":
+        return "Please type something so we can chat :("
+
+    # If there is no good response, return a random one.
+    if best_response != 0:
+        return response_data[response_index]["bot_response"]
+
+    return random_responses.random_string()
+
+
+@app.route("/") #home
+def hello():
+	return render_template("home.html")
+
+@app.route('/logon')
+def logon():
+	return render_template('signup.html')
+
+@app.route('/login')
+def login():
+	return render_template('signin.html')
+
+@app.route("/signup")
+def signup():
+
+    username = request.args.get('user','')
+    name = request.args.get('name','')
+    email = request.args.get('email','')
+    number = request.args.get('mobile','')
+    password = request.args.get('password','')
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
+    con.commit()
+    con.close()
+    return render_template("signin.html")
+
+@app.route("/signin")
+def signin():
+
+    mail1 = request.args.get('user','')
+    password1 = request.args.get('password','')
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("select `user`, `password` from info where `user` = ? AND `password` = ?",(mail1,password1,))
+    data = cur.fetchone()
+
+    if data == None:
+        return render_template("signin.html")    
+
+    elif mail1 == 'admin' and password1 == 'admin':
+        return render_template("form.html")
+
+    elif mail1 == str(data[0]) and password1 == str(data[1]):
+        return render_template("form.html")
+    else:
+        return render_template("signup.html")
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+##while True:
+##    user_input = input("You: ")
+##    print("Bot:", get_response(user_input))
+    
+# start() 
+if __name__=="__main__":
+	app.run(debug=True)
